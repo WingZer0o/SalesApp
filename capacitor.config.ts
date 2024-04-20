30379d3fbaf272ef6e67fb2069afe0c315b255e7
WingZer0o <mtmulch0191@outlook.com>
2024-04-20 14:18:09 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SalesApp',
+  webDir: 'www'
+};
+
+export default config;
