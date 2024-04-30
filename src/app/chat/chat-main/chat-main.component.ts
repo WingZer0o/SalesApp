91d3c10ab3d897c148f947cce6ace95222e83c18
Mike Mulchrone <mtmulch0191@outlook.com>
2024-04-29 20:53:50 -0400

    basic typing animation

--- src/app/chat/chat-main/chat-main.component.ts
+++ src/app/chat/chat-main/chat-main.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, Type, ViewChildren } from '@angular/core';
 import { IonTitle, IonToolbar, IonHeader, IonContent } from "@ionic/angular/standalone";
 import { ChatMessage } from 'src/app/models/chat-main/chat-message';
 import { MaterialModule } from 'src/app/modules/material.module';
 import { SharedModule } from 'src/app/modules/shared.module';
+import { Typewriter } from 'src/app/shared/typewriter/typewriter';
 
 @Component({
   selector: 'app-chat-main',
@@ -12,13 +13,26 @@ import { SharedModule } from 'src/app/modules/shared.module';
   imports: [IonContent, IonHeader, IonToolbar, IonTitle, MaterialModule, SharedModule]
 })
 export class ChatMainComponent  implements OnInit {
+  @ViewChildren('chatMessage')
+  chatMessage!: QueryList<ElementRef>;
 
   public chatMessages: ChatMessage[] = [];
 
   constructor() { }
 
   ngOnInit() {
-    this.chatMessages.push(new ChatMessage(1, "Hello", "John Doe", "Mike Mulchrone", new Date()));
+    this.chatMessages.push(new ChatMessage(1, "Hello there I came acorss this, blahg blahg blahg blahg blahg blahg blahg blahg blahg blahg blahg blahg blahg blahg blahg blahg blahg blahg ", "John Doe", "Mike Mulchrone", new Date()));
   }
 
+
+  public animateChat(message: string, chatIndex: number) {
+    if (this.chatMessage) {
+      const elementRef = this.chatMessage.find((element, index) => index === chatIndex)?.nativeElement;
+      if (elementRef.getAttribute('isChatTyped') === "false") {
+        elementRef.setAttribute('isChatTyped', true);
+        const typeWriter = new Typewriter(elementRef, { loop: false, typingSpeed: 25, deletingSpeed: 50 });
+        typeWriter.typeString(message).start();
+      }
+    }
+  }
 }
--- src/app/models/chat-main/chat-message.ts
+++ src/app/models/chat-main/chat-message.ts
@@ -4,12 +4,14 @@ export class ChatMessage {
     sender: string;
     receiver: string;
     timestamp: Date;
+    isChatBot: boolean;
   
-    constructor(id: number, message: string, sender: string, receiver: string, timestamp: Date) {
+    constructor(id: number, message: string, sender: string, receiver: string, timestamp: Date, isChatBot: boolean = false) {
       this.id = id;
       this.message = message;
       this.sender = sender;
       this.receiver = receiver;
       this.timestamp = timestamp;
+      this.isChatBot = isChatBot;
     }
 }
