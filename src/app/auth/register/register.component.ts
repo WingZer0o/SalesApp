150564cb0ea57c4e73678bad1cb9457f0d92d351
Mike Mulchrone <mtmulch0191@outlook.com>
2024-05-03 22:25:59 -0400

    register page hooked up.

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,29 +1,70 @@
-import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { IonTitle, IonToolbar, IonHeader, IonContent, IonGrid, IonRow, IonCol } from "@ionic/angular/standalone";
-import { MaterialModule } from 'src/app/modules/material.module';
-import { Typewriter } from 'src/app/shared/typewriter/typewriter';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
+import {
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/angular/standalone";
+import { RegisterUserRequestDto } from "src/app/models/register/register-user-dto";
+import { MaterialModule } from "src/app/modules/material.module";
+import { RegisterHttpService } from "src/app/services/http/register-http.service";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss'],
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"],
   standalone: true,
-  imports: [IonCol, IonRow, IonGrid, IonContent, IonHeader, IonToolbar, IonTitle, ReactiveFormsModule, MaterialModule ]
+  imports: [
+    IonCol,
+    IonRow,
+    IonGrid,
+    IonContent,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    ReactiveFormsModule,
+    MaterialModule,
+  ],
 })
 export class RegisterComponent implements OnInit {
-
+  public isLoading: boolean = false;
   public form!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private registerHttpService: RegisterHttpService,
+  ) {}
 
   ngOnInit() {
     this.form = this.formBuilder.group({
-      email: ['', [Validators.required]],
-      password: ['', [Validators.required]]
+      email: ["", [Validators.required]],
+      password: ["", [Validators.required]],
     });
   }
 
-  public handleRegisterUser(): void {
+  public async handleRegisterUser(): Promise<void> {
+    try {
+      if (this.form.valid) {
+        this.isLoading = true;
+        let dto = new RegisterUserRequestDto(
+          this.form.get("email")?.value,
+          this.form.get("password")?.value,
+        );
+        const response = await this.registerHttpService.registerUser(dto);
+        this.form.reset();
+        this.isLoading = false;
+      }
+    } catch (error: any) {
+      console.error(error.message);
+    }
   }
 }
